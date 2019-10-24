b92514cd75c2e0c8f2f2575d7bbd34c656937859
eamanu <eamanu@eamanu.com>
2019-10-23 21:08:51 -0300

    remove unnecesary files

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(mitogen):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="mitogen" command="/usr/bin/mitogen"
--- debian/mitogen.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the mitogen package
-#
-0 4	* * *	root	[ -x /usr/bin/mitogen_maintenance ] && /usr/bin/mitogen_maintenance
